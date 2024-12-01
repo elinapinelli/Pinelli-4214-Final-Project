a00b07f8b3303ed7fa02e39a2b2f573da29f71f9
Elina Pinelli <s.pinelli@acg.edu>
2024-12-01 23:50:45 +0200

    connected products with the site. created products.html and product.html. showed my products to my site. created the search bar. made the search bar to work properly even for example the user writes rose or ros or ROSE the result will be all the products that have the rose word in the title, description, category and sab-category

--- pinelli_site/urls.py
+++ pinelli_site/urls.py
@@ -5,4 +5,7 @@ from . import views#bazo ta views gia kathe endpoint
 urlpatterns=[#pinakas pou tha baloume ola ta endpoints
     path("",views.home,name="home"),
     path("about/",views.about,name="about"),
+    path("products/",views.products,name="products"),
+    path("product/<int:id>/",views.product,name="product"),
+    path("search/",views.search,name="search"),
 ]
--- pinelli_site/views.py
+++ pinelli_site/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse #HttpResponse-> metatrepei ta keimena se html
+from .models import Product
+from django.db.models import Q
 
 # Create your views here.
 
@@ -8,3 +10,22 @@ def home(request):#sinartisi pou servirei tin kentriki selida tou site
 
 def about(request):#sinartisi pou servirei tin kentriki selida tou site
     return render(request,"about.html")
+
+def products(request):#sinartisi pou servirei tin products selida tou site
+    product_list=Product.objects.all()#pairno ta products apo database
+    return render(request,"products.html",{"products":product_list})
+
+def product(request,id):#sinartisi pou servirei tin products selida tou site
+    product_by_id=Product.objects.get(id=id)#i take from my database the id that the URL has
+    return render(request,"product.html",{"product":product_by_id})
+
+def search(request):#sinartisi pou servirei tin products selida tou site
+    # if the user search a word that includes indoors or outdoors it will search for True or False otherwise if the title, description, category and sub category contains the keyword 
+    keyword=request.POST["keyword"]
+    if "indoors".find(keyword)>-1:
+        product_list=Product.objects.filter(indoors=True)    
+    elif "outdoors".find(keyword)>-1:
+        product_list=Product.objects.filter(indoors=False)  
+    else:
+        product_list=Product.objects.filter(Q(title__icontains=keyword) | Q(description__icontains=keyword) | Q(category__icontains=keyword) | Q(sub_category__icontains=keyword) )
+    return render(request,"products.html",{"products":product_list})
